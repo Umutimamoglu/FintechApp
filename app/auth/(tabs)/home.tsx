2d186711174521748a4cf4cbfc0e991d62739fc5
umut imamoglu <umutimamoglu19@gmail.com>
2024-08-28 11:56:04 +0300

    mmkv and zustand

--- app/auth/(tabs)/home.tsx
+++ app/auth/(tabs)/home.tsx
@@ -1,12 +1,114 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, Button } from 'react-native'
 import React from 'react'
+import Colors from '@/constants/Colors';
+import RoundBtn from '@/components/RoundBtn';
+import Dropdown from '@/components/Dropdown';
+import { useBalanceStore } from '@/store/balanceStore';
+import { useHeaderHeight } from '@react-navigation/elements';
+import { defaultStyles } from '@/constants/Styles';
+import { Ionicons } from '@expo/vector-icons';
+
 
 const Page = () => {
+    const { balance, runTransaction, transactions, clearTransactions } = useBalanceStore();
+    const headerHeight = useHeaderHeight();
+
+    const onAddMoney = () => {
+        runTransaction({
+            id: Math.random().toString(),
+            amount: Math.floor(Math.random() * 1000) * (Math.random() > 0.5 ? 1 : -1),
+            date: new Date(),
+            title: 'Added money',
+        });
+    };
+
     return (
-        <View>
-            <Text>Page</Text>
-        </View>
+        <ScrollView style={{ backgroundColor: Colors.background }}>
+            <View style={styles.account}>
+                <View style={styles.row}>
+                    <Text style={styles.balance}>{balance()}</Text>
+                    <Text style={styles.currency}>TL</Text>
+                </View>
+            </View>
+
+
+            <View style={styles.actionRow}>
+                <RoundBtn icon={'add'} text={'Add money'} onPress={onAddMoney} />
+                <RoundBtn icon={'refresh'} text={'Exchange'} onPress={clearTransactions} />
+                <RoundBtn icon={'list'} text={'Details'} />
+                <Dropdown />
+            </View>
+
+            <Text style={defaultStyles.sectionHeader}>Transactions</Text>
+            <View style={styles.transactions}>
+                {transactions.length === 0 && (
+                    <Text style={{ padding: 14, color: Colors.gray }}>No transactions yet</Text>
+                )}
+                {transactions.map((transaction) => (
+                    <View
+                        key={transaction.id}
+                        style={{ flexDirection: 'row', alignItems: 'center', gap: 16 }}>
+                        <View style={styles.circle}>
+                            <Ionicons
+                                name={transaction.amount > 0 ? 'add' : 'remove'}
+                                size={24}
+                                color={Colors.dark}
+                            />
+                        </View>
+
+                        <View style={{ flex: 1 }}>
+                            <Text style={{ fontWeight: '400' }}>{transaction.title}</Text>
+                            <Text style={{ color: Colors.gray, fontSize: 12 }}>
+                                {transaction.date.toLocaleString()}
+                            </Text>
+                        </View>
+                        <Text>{transaction.amount}€</Text>
+                    </View>
+                ))}
+            </View>
+
+
+        </ScrollView>
     )
 }
-
-export default Page
+const styles = StyleSheet.create({
+    account: {
+        margin: 80,
+        alignItems: 'center',
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        gap: 10,
+    },
+    balance: {
+        fontSize: 50,
+        fontWeight: 'bold',
+    },
+    currency: {
+        fontSize: 20,
+        fontWeight: '500',
+    },
+    actionRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 20,
+    },
+    transactions: {
+        marginHorizontal: 20,
+        padding: 14,
+        backgroundColor: '#fff',
+        borderRadius: 16,
+        gap: 20,
+    },
+    circle: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        backgroundColor: Colors.lightGray,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+export default Page;
